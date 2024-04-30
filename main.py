ccd59fe85152394cc036f0bc478895d542e2eaa6
AnnaGiricz <a00747538@unet.univie.ac.at>
2024-04-30 11:11:50 +0200

    submit assessment

--- main.py
+++ main.py
@@ -2,14 +2,35 @@
 Simple Calculator
 """
 # Provide your solution here
+def calculate (a, b):
+    return a + b
+
+
+print(calculate(3,4)
+
+
+
+
 
 
 """
 Reverse Word
 """
 # Provide your solution here
+word = str
+def word (word):
+    print(word.capitalize())
+
+def reverse_word (word):
+    str(word)
+    result = []
+
+
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
-
